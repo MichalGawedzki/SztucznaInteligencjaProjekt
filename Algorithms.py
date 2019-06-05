a5f19d7f7e482561de6ceda7a6f5427cf3045bc9
Michal <michal.gawedzki98@gmail.com>
2019-06-05 17:15:54 +0200

    bug in maze_generator

--- Algorithms.py
+++ Algorithms.py
@@ -1,8 +1,9 @@
-import Maze
+import generator
 import queue
 from random import randrange, shuffle, random
 import random
 import numpy as np
+from copy import copy, deepcopy
 
 class Point:
     def __init__(self, x, y):
@@ -17,66 +18,95 @@ class Maze:
         # self.s_y = s_y
         # self.f_x = f_x
         # self.f_y = f_y
-        self.grid = grid
+        self.dfsLen = 0
+        self.bfsLen = 0
+        self.GRID = grid
         self.s = Point(s_x, s_y)
         self.f = Point(f_x, f_y)
         self.Path = []
-        self.M = len(self.grid[0])
-        self.N = len(self.grid)
+        self.M = len(self.GRID[0])
+        self.N = len(self.GRID)
         self.ifVisited = [[0 for x in range(self.M)] for y in range(self.N)]  # 2d array telling if we already visited particular block
 
+    def get_dfsLen(self):
+        return self.dfsLen
+
+    def get_bfsLen(self):
+        return self.bfsLen
+
+    def prepare(self):
+        self.Path = []
+        self.ifVisited = [[0 for x in range(self.M)] for y in range(self.N)]  # 2d array telling if we already visited particular block
+
+
     # recursive search with DFS (up -> right -> down -> left)
-    def dfs(self, i, j):
+    def d_dfs(self, i, j):
 
-        # adds to queue
-        #if self.grid[self.s.x][self.s.y] != 'S':
-        self.Path.append([i, j])
-        self.ifVisited[i][j] = 1
+        self.prepare()
+        grid = deepcopy(self.GRID)
+        
+        def dfs(i, j):
 
-        if self.grid[i][j] == "F":
-            # print("Found solution")
-            return True # 'True' ends dfs and begins to roll recursion back
+            # adds to queue
+            #if self.grid[self.s.x][self.s.y] != 'S':
+            self.Path.append([i, j])
+            self.ifVisited[i][j] = 1
 
-        # up
+            if grid[i][j] == "F":
+                # print("Found solution")
+                return True # 'True' ends dfs and begins to roll recursion back
 
-        if 0 <= i - 1 <= self.N - 1 and 0 <= j <= self.M - 1: # if block is still in maze's height and width
-            if self.grid[i - 1][j] != "X" and self.ifVisited[i - 1][j] == 0: # if block is not 'X' and if not visited before
-                # print("u -> ", end = '')
-                if self.dfs(i - 1, j) == True:
-                    self.grid[i][j] = "1"
-                    self.grid[self.s.x][self.s.y] = "S"
-                    return True
 
-        # right
-        if 0 <= i <= self.N - 1 and 0 <= j + 1 <= self.M - 1:
-            if self.grid[i][j + 1] != "X" and self.ifVisited[i][j + 1] == 0:
-                # print("r -> ", end = '')
-                if self.dfs(i, j + 1) == True:
-                    self.grid[i][j] = "1"
-                    self.grid[self.s.x][self.s.y] = "S"
-                    return True
+        
+            # up
+            if 0 <= i - 1 <= self.N - 1 and 0 <= j <= self.M - 1: # if block is still in maze's height and width
+                if grid[i - 1][j] != "X" and self.ifVisited[i - 1][j] == 0: # if block is not 'X' and if not visited before
+                    # print("u -> ", end = '')
+                    if dfs(i - 1, j) == True:
+                        grid[i][j] = "1"
+                        return True
 
-        # down
-        if 0 <= i + 1 <= self.N - 1 and 0 <= j <= self.M - 1:
-            if self.grid[i + 1][j] != "X" and self.ifVisited[i + 1][j] == 0:
-                # print("d -> ", end = '')
-                if self.dfs(i + 1, j) == True:
-                    self.grid[i][j] = "1"
-                    self.grid[self.s.x][self.s.y] = "S"
-                    return True
+            # right
+            if 0 <= i <= self.N - 1 and 0 <= j + 1 <= self.M - 1:
+                if grid[i][j + 1] != "X" and self.ifVisited[i][j + 1] == 0:
+                    # print("r -> ", end = '')
+                    if dfs(i, j + 1) == True:
+                        grid[i][j] = "1"
+                        return True
 
-        # left
-        if 0 <= i <= self.N - 1 and 0 <= j - 1 <= self.M - 1:
-            if self.grid[i][j - 1] != "X" and self.ifVisited[i][j - 1] == 0:
-                # print("l -> ", end = '')
-                if self.dfs(i, j - 1) == True:
-                    self.grid[i][j] = "1"
-                    self.grid[self.s.x][self.s.y] = "S"
-                    return True
-        # Removes from queue
-        self.Path.pop()
-        self.grid[self.s.x][self.s.y] = "S"
-        return False
+            # down
+            if 0 <= i + 1 <= self.N - 1 and 0 <= j <= self.M - 1:
+                if grid[i + 1][j] != "X" and self.ifVisited[i + 1][j] == 0:
+                    # print("d -> ", end = '')
+                    if dfs(i + 1, j) == True:
+                        grid[i][j] = "1"
+                        return True
+
+            # left
+            if 0 <= i <= self.N - 1 and 0 <= j - 1 <= self.M - 1:
+                if grid[i][j - 1] != "X" and self.ifVisited[i][j - 1] == 0:
+                    # print("l -> ", end = '')
+                    if dfs(i, j - 1) == True:
+                        grid[i][j] = "1"
+                        return True
+            # Removes from queue
+            self.Path.pop()
+            return False
+
+        
+        dfs(i,j)
+        grid[self.s.x][self.s.y] = "S"
+        if len(self.Path)>0:    
+            self.Path.pop(0)
+        self.dfsLen = len(self.Path) - 1
+
+        # print("GRID: ")
+        # print(self.GRID)
+        # print("grid: ")
+        # print(grid)
+
+        print(self.dfsLen)
+
 
     def dfsRandom(self, i, j):
 
@@ -183,7 +213,8 @@ class Maze:
         self.grid[self.f.x][self.f.y] = "F"
 
 
-def maze_generator(N=18):
+def maze_generator(N):
+
     loop_counter = 0
     breadth, width = N, N
     maze = [['X' for _ in range(N)] for _ in range(N)]
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-import Maze
+import generator
 import Algorithms
 from pprint import pprint
 
 boards = []
-boards.append(Maze.board1)
+boards.append(generator.board1)
 
-new_maze = Algorithms.maze_generator()
+new_maze = Algorithms.maze_generator(18)
 boards.append(new_maze)
-new_maze.bfs(new_maze.s.x, new_maze.s.y)
-Maze.draw_maze(new_maze.grid, new_maze.Path)
+#new_maze.d_dfs(new_maze.s.x, new_maze.s.y)
+#a = new_maze.get_dfsLen()
+
+for x in range(100):
+        new_maze = Algorithms.maze_generator(18)
+        new_maze.d_dfs(new_maze.s.x, new_maze.s.y)
+        a = new_maze.get_dfsLen()
+        if a == -1:
+                print("caught")
+                break
+
+generator.draw_maze(new_maze.GRID, new_maze.Path)
+
+# new_maze = Algorithms.maze_generator()
+# boards.append(new_maze)
+# new_maze.dfs(new_maze.s.x, new_maze.s.y)
+# Maze.draw_maze(new_maze.grid, new_maze.Path)
